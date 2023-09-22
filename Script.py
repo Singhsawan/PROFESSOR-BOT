3a56becf4ba00a700c9c2c4c3caa2b19d12b019c
Jack <125857863+Singhsawan@users.noreply.github.com>
2023-09-22 07:50:38 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -336,7 +336,24 @@ Delete A Lot Of Messages From Groups!
 <code>/video https://youtu.be/*****</code>  """
 
 
-    
+   IMDB_TEMPLATE = """
+<b>Query: {qurey}
+
+IMDb Data:
+
+<b>🏷 Title</b>: <a href={url}>{title}</a>
+🎭 Genres: {genres}
+📆 Year: <a href={url}/releaseinfo>{year}</a>
+🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
+☀️ Languages : <code>{languages}</code>
+📀 RunTime: {runtime} Minutes
+📆 Release Info : {release_date}
+🎛 Countries : <code>{countries}</code>
+
+
+⏰Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
+
+Requested by : {message.from_user.mention}</b>"""   
 
 
     
